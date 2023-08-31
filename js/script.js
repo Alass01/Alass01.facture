bf4f0a7781bbafc16ea1de3e15d932cd5d88b4a1
Alass01 <118843942+Alass01@users.noreply.github.com>
2023-08-31 16:44:01 +0000

    test 1

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const preloaderWreapper = document.querySelector('.preloader-wreapper');
+
+window.addEventListener('load', function(){
+    // preloaderWreapper.classList.add('fad-out-animation')
+});
